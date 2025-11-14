02e52c4fa9fc41e68684e6489d038f21cbf0f05d
Vikaskumarn1 <143833684+Vikaskumarn1@users.noreply.github.com>
2025-11-14 18:19:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,18 @@ const nextConfig = {
   allowedDevOrigins: ['192.168.2.2:3000', 'localhost:3000', '127.0.0.1:3000'],
   reactStrictMode: true,
   images: {
-    domains: ['fakestoreapi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        pathname: '/img/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.fakestoreapi.com',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
-    unoptimized: false,
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://fakestoreapi.com',
   },
 }
 
